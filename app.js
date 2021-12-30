80c9c394986696567e581c944d5bb817f54ac5fc
Spawar1530 <spawar1530@gmail.com>
2021-12-30 14:33:12 +0530

    complete application with data file

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
+const path = require('path');
+
 const express = require('express');
-const path = require("path");
-
-const adminRoutes = require("./routes/admin")
-
-const userRoutes = require('./routes/shop')
-
 const bodyParser = require('body-parser');
 
-const productsController = require("./controllers/products")
-
-const errorController = require("./controllers/error")
+const errorController = require('./controllers/error');
 
 const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended:false}))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin',adminRoutes)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(userRoutes)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(errorController.get404);
 
-app.use(errorController.get404)
-
-app.listen(3000)
+app.listen(3000);
--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,3 @@
-const { restart } = require("nodemon")
-
-exports.get404 = (req, res,next) => {
-    res.status(404).render("404.ejs", {pageTitle: "Page Not Found"})
-}
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
+};
