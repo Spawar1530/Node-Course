2c08554652214b9d43dfe445a4921354f0c9ba46
Spawar1530 <spawar1530@gmail.com>
2022-01-06 15:07:33 +0530

    complete application with mongoose

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('61d575a2fc8df60c7452a6b1')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
+User.findById("61d696b81c889951d589c9b8").
+then(user => {
+  req.user = user
+  next()
+})
+  .catch(err => console.log(err))
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://suraj:Test12345@cluster0.f97ut.mongodb.net/Node_Course?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    User.findOne().then(user => {
+      if(!user){
+        const user = new User({
+          name: "Suraj",
+          email: "suraj@gmail.com",
+          cart:{
+            items: [
+      
+            ]
+          }
+        })
+        user.save()
+      }
+    })
+    app.listen(3000);
+    console.log("connected")
+  })
+  .catch((err) => console.log(err));
